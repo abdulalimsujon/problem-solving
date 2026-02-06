edea0a7ed1c61a2486d672b4a17e72fbe5d36d57
abdulalimsujon <alimsujon12@gmail.com>
2026-02-06 12:11:01 +0600

    set precision

---
+++ setprecision.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    double r;
+    cin >> r;
+    double area = 3.141592653 * r * r;
+    cout  << fixed << setprecision(9) << area << "\n";
+
+    return 0;
+}
