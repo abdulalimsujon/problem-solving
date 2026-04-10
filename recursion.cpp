9c881a9a3afa8b86a1c8532a59d5cf0b19a39700
abdulalimsujon <alimsujon12@gmail.com>
2026-04-10 16:50:43 +0600

    palindrome with recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,29 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void recursion(int n){
-    if(n==5)
-        return;
-    recursion(n+1);
-    cout << " " << n;
+// void recursion(int n){
+//     if(n==5)
+//         return;
+//     recursion(n+1);
+//     cout << " " << n;
     
-}
-int add(int n){
-    if(n>5){
-        return 0;
-    }
-    int sum = add(n + 1);
+// }
+// int add(int n){
+//     if(n>5){
+//         return 0;
+//     }
+//     int sum = add(n + 1);
 
-    return sum + n;
+//     return sum + n;
+// }
+
+bool palindrome(vector<int> &a, int l, int r) {
+    if (l >= r) {
+        return true;
+    }
+
+    if (a[l] != a[r]) {
+        return false;
+    }
+
+    return palindrome(a, l + 1, r - 1);
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // recursion = recursive + optimization
+    int n;
+    cin >> n;
 
-   cout << "ans:" << add(1);
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    cout << "ans: " << palindrome(v, 0, v.size() - 1);
 
     return 0;
 }
