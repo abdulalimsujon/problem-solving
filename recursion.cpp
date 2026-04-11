42514e7db93939c9bbce9f070ee9dc7d7a514160
abdulalimsujon <alimsujon12@gmail.com>
2026-04-11 11:20:39 +0600

    recursion with memoization

--- recursion.cpp
+++ recursion.cpp
@@ -29,6 +29,14 @@ bool palindrome(vector<int> &a, int l, int r) {
     return palindrome(a, l + 1, r - 1);
 }
 
+int fibo(int n){
+         
+    if(n<2){
+        return n;
+    }
+    return fibo(n-1) + fibo(n - 2);
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -36,12 +44,12 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
+    // vector<int> v(n);
+    // for (int i = 0; i < n; i++) {
+    //     cin >> v[i]
+    // }
 
-    cout << "ans: " << palindrome(v, 0, v.size() - 1);
+    cout << fibo(n);
 
     return 0;
 }
